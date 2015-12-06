f99f6adcd75c7c4335ac7e4e4969c4ad9a210238
akaFTS <Gustavo@MacBook-Pro-de-Gustavo.local>
2015-12-06 13:18:19 -0200

    Testando bot.

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "oi amigos";
+require 'vendor/autoload.php';
+use Telegram\Bot\Api;
+
+
+$telegram = new Api('173649343:AAGzLaPRv_6_NIrkFguAUI2hOQZM3WEUdKU');
+
+// Standalone
+$me = $telegram->getMe();
+
+$botId = $me->getId();
+$firstName = $me->getFirstName();
+$username = $me->getUsername();
+
+error_log($firstName." - ".$username." - ".$botId);
