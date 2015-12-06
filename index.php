a45cd8da41a0a9d2db5c437683e5fbfa8bd81b1c
akaFTS <Gustavo@MacBook-Pro-de-Gustavo.local>
2015-12-06 13:35:38 -0200

    Testando resposta

--- index.php
+++ index.php
@@ -10,5 +10,7 @@ $telegram->setWebhook('https://cantareirabot.herokuapp.com/index.php');
 $updates = $telegram->getWebhookUpdates();
 
 foreach ($updates as $up) {
-	error_log(print_r($up, true));
+	$amigo = $up['from']['first_name'];
+	$cid = $up['chat']['id'];
+	$telegram->sendMessage($cid, "Oi pra voce tambem amigo ".$amigo."!");
 }
