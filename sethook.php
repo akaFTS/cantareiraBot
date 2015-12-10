38125605e7acf706688bfbbea76e95a63e8df445
akaFTS <Gustavo@MacBook-Pro-de-Gustavo.local>
2015-12-10 00:51:54 -0200

    Teste 3

--- sethook.php
+++ sethook.php
@@ -1,11 +1,11 @@
 <?php
 
-require __DIR__.'/vendor/autoload.php';
+require 'vendor/autoload.php';
 use Telegram\Bot\Api;
 
-require __DIR__.'/comandos/StartCommand.php';
+require 'comandos/StartCommand.php';
 
 
 $telegram = new Api('173649343:AAGzLaPRv_6_NIrkFguAUI2hOQZM3WEUdKU');
 $telegram->setWebhook('https://cantareirabot.herokuapp.com/proccess.php');
-$telegram->addCommand(Telegram\Bot\Commands\StartCommand::class);
+$telegram->addCommand(StartCommand::class);
