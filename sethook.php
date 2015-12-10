0586b428361a099f13a8d2387903d090475089f8
akaFTS <Gustavo@MacBook-Pro-de-Gustavo.local>
2015-12-10 00:46:38 -0200

    Teste 2

--- sethook.php
+++ sethook.php
@@ -1,9 +1,9 @@
 <?php
 
-require 'vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 use Telegram\Bot\Api;
 
-require 'comandos/StartCommand.php';
+require __DIR__.'/comandos/StartCommand.php';
 
 
 $telegram = new Api('173649343:AAGzLaPRv_6_NIrkFguAUI2hOQZM3WEUdKU');
