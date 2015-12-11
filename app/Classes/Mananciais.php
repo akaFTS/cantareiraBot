723ad31ed405e18543f4fdb930d125596124f2bb
akaFTS <Gustavo@MacBook-Pro-de-Gustavo.local>
2015-12-11 17:25:55 -0200

    Primeiro teste com informação real

--- app/Classes/Mananciais.php
+++ app/Classes/Mananciais.php
@@ -9,20 +9,18 @@ class Mananciais {
 	
 	public static function getChuvas(){
 
-		// $crawler = $client->request('GET', 'http://www2.sabesp.com.br/mananciais/DivulgacaoSiteSabesp.aspx');
-  //       $tabela = $crawler->filter("table.tabDados");
-  //       $cells = $tabela->filter(".guardaImgBgDetalhe");
+		$client = new Client();
+		$crawler = $client->request('GET', 'http://www2.sabesp.com.br/mananciais/DivulgacaoSiteSabesp.aspx');
+        $cells = $crawler->filter("table.tabDados")->filter(".guardaImgBgDetalhe");
 		$chuvas = array();
 		$obj = new stdClass();
 		$obj->nome = "Cantareira";
-		$obj->cod = "cnt";
-		$obj->hoje = "4.2mm";
+		$obj->hoje = $cells->eq(1);
 		$obj->acum = "90mm";
 		$obj->media = "200mm";
 		$chuvas[] = $obj;
 		$obj = new stdClass();
 		$obj->nome = "Alto Tietê";
-		$obj->cod = "alt";
 		$obj->hoje = "0.2mm";
 		$obj->acum = "40mm";
 		$obj->media = "195mm";
