c3e17993b24c3068706f7450d73a2cac339dc16f
akaFTS <Gustavo@MacBook-Pro-de-Gustavo.local>
2015-12-11 17:40:23 -0200

    Todas informações reais adicionadas

--- app/Classes/Mananciais.php
+++ app/Classes/Mananciais.php
@@ -13,18 +13,15 @@ class Mananciais {
 		$crawler = $client->request('GET', 'http://www2.sabesp.com.br/mananciais/DivulgacaoSiteSabesp.aspx');
         $cells = $crawler->filter("table.tabDados")->filter(".guardaImgBgDetalhe");
 		$chuvas = array();
-		$obj = new stdClass();
-		$obj->nome = "Cantareira";
-		$obj->hoje = $cells->eq(1)->text();
-		$obj->acum = "90mm";
-		$obj->media = "200mm";
-		$chuvas[] = $obj;
-		$obj = new stdClass();
-		$obj->nome = "Alto Tietê";
-		$obj->hoje = "0.2mm";
-		$obj->acum = "40mm";
-		$obj->media = "195mm";
-		$chuvas[] = $obj;
+		$nomes = ["Cantareira", "Alto Tietê", "Guarapiranga", "Alto Cotia", "Rio Grande", "Rio Claro"];
+		for($i = 0; $i < 6; $i++){
+			$obj = new stdClass();
+			$obj->nome = $nomes[$i];
+			$obj->hoje = $cells->eq($i + 1)->text();
+			$obj->acum = $cells->eq($i + 2)->text();
+			$obj->media = $cells->eq($i + 3)->text();
+			$chuvas[] = $obj;
+		}
 		return $chuvas;
 
 	}
